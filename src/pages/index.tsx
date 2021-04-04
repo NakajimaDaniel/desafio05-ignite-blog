c27a0e808944a48cf68e4a2a4880fc34cd2a90b8
NakajimaDaniel <danieln17.contato@outlook.com>
2021-04-04 16:09:49 -0300

    carregamento dos posts do prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
-
+import Prismic from '@prismicio/client';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -24,12 +25,51 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ posts }: Post) {
 
-export const getStaticProps = async () => {
+  console.log(posts)
+  return (
+    <div>
+      {posts.map(posts=> (
+        <h1>{posts.uid}</h1>
+      ))}
+    </div>
+
+  )
+
+}
+
+export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
-  const postsResponse = await prismic.query(TODO);
+  
 
+  const postsResponse = await prismic.query([
+    Prismic.Predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.title', 'posts.subtitle', 'posts.author','posts.content'],
+    pageSize: 20,
+  });
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+
+      first_publication_date: new Date(post.first_publication_date).toLocaleDateString('pt-br', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+      
+    }
+  })  
+
+
+  return {
+    props: {posts}
+  }
 };
